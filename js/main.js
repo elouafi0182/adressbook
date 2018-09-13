8405840d1a135bd559da96219cf1122ef38ae273
elouafi0182 <you@example.com>
2018-09-13 10:17:47 +0200

    made files

--- js/main.js
+++ js/main.js
@@ -1,7 +1,20 @@
-$.ajax({
-    url: 'https://randomuser.me/api/',
-    dataType: 'json',
-    success: function(data) {
-        console.log(data);
-    }
+$(document).ready(function () {
+
+    $.ajax({
+        url: 'http://api.randomuser.me/?results=10&seed=foobar',
+        dataType: 'json',
+        success: function (data) {
+            for ( var i = 0; i < data.results.length; i++ ) {
+                console.log(data.results[i].name.first);
+                $('#example tbody').append("<tr></tr>");
+                $('#example tbody tr:last-of-type')
+                    .append("<td>" + data.results[i].name.first + " " + data.results[i].name.last + "</td>")
+                    .append("<td>" + data.results[i].email + "</td>")
+                    .append("<td>" + data.results[i].cell + "</td>")
+                    .append("<td>" + "<IMG SRC='" +  data.results[i].picture.medium + "'></td>");
+
+            }
+        }
+    });
+
 });
